d7966475895ba9b53e48c1e6ed58d772cfb66759
Denver <audgkwns44@gmail.com>
2026-01-16 20:36:34 +0900

    [Create]Section Component

---
+++ components/Section.tsx
@@ -0,0 +1,36 @@
+import { Box, Container, Typography } from "@mui/material";
+
+export default function Section(props: {
+  eyebrow?: string;
+  title: string;
+  desc?: string;
+  children?: React.ReactNode;
+}) {
+  const { eyebrow, title, desc, children } = props;
+
+  return (
+    <Box
+      component={"section"}
+      sx={{
+        py: { xs: 6, md: 10 },
+      }}
+    >
+      <Container maxWidth="lg">
+        {eyebrow && <Typography variant="overline">{eyebrow}</Typography>}
+        <Typography variant="h3" sx={{ fontWeight: 800, mt: 1 }}>
+          {title}
+        </Typography>
+        {desc && (
+          <Typography
+            variant="body1"
+            color="text.secondary"
+            sx={{ mt: 2, maxWidth: 720 }}
+          >
+            {desc}
+          </Typography>
+        )}
+        {children && <Box sx={{ mt: 4 }}>{children}</Box>}
+      </Container>
+    </Box>
+  );
+}
