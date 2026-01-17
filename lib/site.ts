c82eb517286a45f6e1252d47695bcd49c235fd0d
Denver <audgkwns44@gmail.com>
2026-01-17 23:06:43 +0900

    [Configure]Update site url for metadata

--- lib/site.ts
+++ lib/site.ts
@@ -2,7 +2,7 @@
 export const site = {
   name: "정청 아지트",
   description: "정의교회 청년부 아지트입니다!",
-  url: "https://foo.web.app/",
+  url: "https://youth-azit.vercel.app/",
   nav: [
     { href: "/", label: "Home", isReady: true },
     // { href: "/manual", label: "Manual", isReady: false },
