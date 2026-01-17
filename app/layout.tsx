49a430684bb93664c770a0cd57bae933bd1444af
Denver <audgkwns44@gmail.com>
2026-01-17 23:09:24 +0900

    [Configure]downsize og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og.png",
+        url: "/og.jpeg",
         width: 1200,
         height: 630,
         alt: "정청아지트",
